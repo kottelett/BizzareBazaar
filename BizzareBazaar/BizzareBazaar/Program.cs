69044a8f518815936d562902b6bfc52af6e6fd51
GitLars <larsdalan@hotmail.com>
2017-11-21 20:53:26 +0100

    Removes comment from Inventory in IManipulateInventory

--- BizzareBazaar/BizzareBazaar/Program.cs
+++ BizzareBazaar/BizzareBazaar/Program.cs
@@ -11,21 +11,7 @@ namespace BizzareBazaar
     {
         public static void Main(string[] args)
         {
-            //Customer peasantCustomer = CustomerFactory.CreateCustomer(CustomerClass.Peasant, "Peasant");
-            //      Customer warriCustomer = CustomerFactory.CreateCustomer(CustomerClass.Warrior, "Big strong Warrior");
-            //      Customer wizardCustomer = CustomerFactory.CreateCustomer(CustomerClass.Wizard, "Scary Wizard");
-            //      ItemProduction prod = new ItemProduction();
-
-            //      peasantCustomer.AddItemsToInventory(prod.GetInventory());
-            //      Console.WriteLine("Peasant inventory:\n");
-            //      peasantCustomer.ShowInventory();
-
-            //      warriCustomer.PrintInformation();
-            //      wizardCustomer.PrintInformation();
-
-            //      Booth.SetTimerAndFetchItems();
-
-
+   
             ItemProduction.SetTimerAndProduceItems();
 			Booth b = new Booth();
 	        b.SetTimerAndFetchItems();
