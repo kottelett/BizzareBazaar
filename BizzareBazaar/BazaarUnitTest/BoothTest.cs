903e413f054c45fc011931ab50c6dd4d48e5aeb6
kottelett <sdahle95@gmail.com>
2017-11-25 20:01:08 +0100

    ItemProductionTest

---
+++ BizzareBazaar/BazaarUnitTest/BoothTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BizzareBazaar;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+
+namespace BazaarUnitTest
+{
+   [TestFixture]    
+   public class BoothTest
+    {
+        
+        [Test]
+        public void boothtest()
+        {
+            Booth booth = new Booth(1, 10);
+        }
+
+
+    }
+}
